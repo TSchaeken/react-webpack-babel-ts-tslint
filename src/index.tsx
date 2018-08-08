a1882c4f9109a905d1c337815e364a97c4cdd87f
TSchaeken-Deskop <TSchaeken@gmail.com>
2018-08-07 20:30:53 -0700

    tslint integrated, added additional airbnb and react rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import Hello from './components/App';
 
-ReactDOM.render(<Hello compiler={'thing'} framework={'thing2'}/>, document.getElementById('root'));
+ReactDOM.render(<Hello compiler={'thing'} framework={'thing2'} />, document.getElementById('root'));
